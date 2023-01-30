d5482cf1784c0cad1b34a838d40dce0717f75b24
Rishabh248 <96188139+Rishabh248@users.noreply.github.com>
2023-01-30 21:09:39 +0530

    Add files via upload

---
+++ w1/q1.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Creating a structure and using typedef for future ease in typing //
+
+typedef struct bst* ptrtobst;
+typedef struct bst {
+    int data;
+    ptrtobst lchild;
+    ptrtobst rchild;
+    }bst;
+
+//initialising the root node//
+ptrtobst root = NULL;
+
+
+//inserting any element in the bst//
+ptrtobst insert(int element, ptrtobst node){
+    ptrtobst temp;
+    if(node == NULL)
+        {
+            temp = (ptrtobst) malloc(sizeof(bst));
+            temp->data = element;
+            temp->lchild = NULL;
+            temp->rchild = NULL;
+                if(root == NULL)
+                    root = temp;
+                return temp;
+                }
+
+
+                else if(element < node->data)
+                {
+                node->lchild = insert(element, node->lchild);
+                return node;
+                }
+                else if(element > node->data)
+                {
+                node->rchild = insert(element, node->rchild);
+                return node;
+                }
+            else
+            {
+            printf("Duplicate Element found while inserting!\n");
+            return NULL;
+        }
+    }
+
+
+//inorder traversal//
+
+void inorder(ptrtobst node)
+{
+    if(node == NULL)
+        return;
+        inorder(node->lchild);
+        printf("%d ",node->data);
+        inorder(node->rchild);
+    }
+
+
+
+//preorder traversal//
+
+void preorder(ptrtobst node)
+{
+        if(node == NULL)
+        return;
+        printf("%d ",node->data);
+        preorder(node->lchild);
+        preorder(node->rchild);
+    }
+
+
+//postorder traversal//
+
+void postorder(ptrtobst node)
+{
+        if(node == NULL)
+        return;
+        postorder(node->lchild);
+        postorder(node->rchild);
+        printf("%d ",node->data);
+    }
+
+
+
+//key search function//
+
+void search(int key,ptrtobst node){
+    if(node==NULL){
+    printf("Key not found!!\n");
+    node = insert(key,root);
+    return;
+    }
+    else{
+    if(key == node->data){
+        printf("our key i.e.'%d' found!\n",key);
+        return;
+    }
+    else if(key>node->data)
+        search(key,node->rchild);
+    else if(key<node->data)
+        search(key,node->lchild);
+    }
+}
+
+
+
+int main(){
+    int ele;
+    int key;
+    int ch = 1;
+    while(ch == 1){
+    printf("Enter Element to Insert in the BST: \n");
+    scanf("%d", &ele);
+    insert(ele, root);
+    
+    printf("Enter 1 to Insert / anything else to Stop : \n");
+    scanf("%d", &ch);
+    }
+
+        printf("\nInorder: \n");
+        printf("Preorder: \n");
+        preorder(root);
+
+        printf("Postorder: \n");
+        postorder(root);
+        inorder(root);
+
+        printf("\nEnter the Element to be Searched in the BST: \n");
+        scanf("%d", &key);
+        search(key,root);
+}
---
+++ w1/q2-1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+int n;
+int arr[n][n];
+// int flag=0;
+
+printf("Enter number of nodes\n");
+scanf("%d",&n);
+
+
+for (int i = 0; i < n; ++i)
+{
+for (int j = 0; j < n; ++j)
+{
+printf("Is there any vertex between %d and %d \n",i+1,j+1);
+printf("If yes press 1\nIf no press 0\n");
+
+scanf("%d", &arr[i][j]);
+
+printf("arr[%d][%d] = %d \n",i,j,arr[i][j]);
+
+
+}
+}
+
+for (int i = 0; i < n; ++i)
+{
+for (int j = 0; j < n; ++j)
+{
+printf("arr[%d][%d] = %d \n",i,j,arr[i][j]);
+}
+}
+return 0;
+}
